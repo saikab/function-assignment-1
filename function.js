5550f020c732914bc25409d151f727ea1c36a0a7
saikab <97691842+saikab@users.noreply.github.com>
2022-08-25 22:05:03 +0530

    assignment for relevel

---
+++ function.js
@@ -0,0 +1,21 @@
+
+
+function stringJoin(a,b,c,d){
+  
+  
+  var newString  =   (  ` You will be a ${a}   in  ${b}    and  married to ${c}  with  ${d} kid .` );
+  
+  console.log(newString);
+
+
+  return ;
+
+
+
+}
+
+
+stringJoin("doctor" ," kolkata " , "Sanu"  , "one ");
+
+
+
